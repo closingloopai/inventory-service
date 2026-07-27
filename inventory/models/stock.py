15a5bea136f059af017fd65e2ccd48c545848871
closingloopai <contact@closingloopai.com>
2026-07-27 12:02:45 +0530

    scaffold inventory/models/stock.py

---
+++ inventory/models/stock.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+@dataclass
+class StockLevel:
+    sku: str
+    on_hand: int
+    reserved: int = 0
+    @property
+    def available(self) -> int:
+        return self.on_hand - self.reserved
